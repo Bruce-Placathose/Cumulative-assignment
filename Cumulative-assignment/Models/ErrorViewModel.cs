3acbeb33d1a1238ff057f89f37f18b3d54db395e
Bruce <bruceplacathose@gmail.com>
2024-11-29 18:24:52 -0500

    first commit

---
+++ Cumulative-assignment/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Cumulative_assignment.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
